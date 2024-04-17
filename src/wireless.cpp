3846819edbcb8ba273ff528fe98fcb37afe375a6
Rafael Ramildes Ferreira <rafael.r.ferreira@grad.ufsc.br>
2024-04-17 17:42:51 -0300

    Fix: Timer Interrupt clas created to wrap ESP8266 ITimer class

--- src/wireless.cpp
+++ src/wireless.cpp
@@ -1,44 +1,8 @@
 #include <ESP8266WiFi.h>
 #include "main.h"
-#include "buildconfig.h"
 #include "wireless.h"
-#include "ESP8266TimerInterrupt.h"
+#include "timerinterruption.h"
 
-// Select a Timer Clock
-#define USING_TIM_DIV1                false           // for shortest and most accurate timer
-#define USING_TIM_DIV16               false           // for medium time and medium accurate timer
-#define USING_TIM_DIV256              true            // for longest timer but least accurate. Default
-
-#define TIMER_INTERVAL_MS       1000
-
-
-// Init ESP8266 timer 1
-ESP8266Timer ITimer;
-
-/**
- * @brief Sets a periadical timer interrupt to send the wireless messages
- * @param func: Function that describes the routine used to send the messages
- * @returns Stauts:
- * 			0: No error
- * 			-1: Error
-*/
-static int set_timer_interrupt(void func(void)){
-	if(!ITimer.attachInterruptInterval(TIMER_INTERVAL_MS * 1000, func)){
-		#ifdef DEBUG
-			Serial.println("ERROR: Periodical Wireless transmition failed");
-		#endif
-		return -1; 
-	}
-	#ifdef DEBUG
-	else {
-		Serial.print("Communication timer configured to ");
-		Serial.print(TIMER_INTERVAL_MS);
-		Serial.println(" msec");
-	}
-	#endif
-
-	return 0; // No error
-}
 
 /* Starts static attributes */
 ChestCompression *WiFiCommunicator::chest = nullptr;
@@ -49,7 +13,7 @@ WiFiServer WiFiCommunicator::server(80);
 */
 WiFiCommunicator::WiFiCommunicator(ChestCompression *chest){
 	// Configures the periodically routine in which messages are sent
-	set_timer_interrupt(send_wifi);
+	TimerInterruption::set_timer_interrupt(send_wifi);
 	
 	// Configurar a ESP8266 como ponto de acesso
 	WiFi.softAP(ssid);
