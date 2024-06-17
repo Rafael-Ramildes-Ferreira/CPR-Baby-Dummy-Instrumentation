aa16aba45fa28507824b09323ed03f1cc9971252
Rafael Ramildes Ferreira <rafael.r.ferreira@grad.ufsc.br>
2024-06-17 10:35:39 -0300

    Dist Sensor readings review, printing error code when DEBUG flag is defined

--- src/chest_compressions.cpp
+++ src/chest_compressions.cpp
@@ -27,21 +27,24 @@ ChestCompression::ChestCompression(){
 double ChestCompression::calc_distance(){
 	// Filter values
 	static double yn = 0, yn1 = 0, yn2 = 0, xn = 0, xn1 = 0;
+
 	// Reads the sensor
 	uint8_t range = dist_sensor.readRange();
 	if (dist_sensor.readRangeStatus() == VL6180X_ERROR_NONE){
 		xn = range;
 	} else {
-		// xn = UINT8_MAX; // Used to be 200 (is that the maximum distance? [todo])
-		return yn; // Ignores failed reading
+		#ifdef DEBUG
+			Serial.print("Distance sensor Error Code: ");
+			Serial.println(dist_sensor.readRangeStatus());
+		#endif
+		return this->distance; // Ignores failed reading
 	}
+
 
 	// Compute the filtered signal
 	yn = 1.656*yn1 - 0.6859*yn2 + 0.01568*xn + 0.01383*xn1;
 
-
 	// Updates values
-	// delay(1); // Why delay?
 	xn1 = xn;
 	yn2 = yn1;
 	yn1 = yn;
