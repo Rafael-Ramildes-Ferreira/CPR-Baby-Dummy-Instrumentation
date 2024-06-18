551c1bee1ffd79b3d0f527a88a7b17e13596aeec
Rafael Ramildes Ferreira <rafael.r.ferreira@grad.ufsc.br>
2024-06-18 15:26:28 -0300

    Old implementation of timer interrupt deleted and a idea of ISR change was added

--- src/timerinterruption.cpp
+++ src/timerinterruption.cpp
@@ -19,28 +19,18 @@ TimerInterruption::TimerInterruption(uint8_t timer_instance){
  * 			-1: Error
 */
 int TimerInterruption::set_timer_interrupt(void func(void)){
-	// if(!TimerInterruption::ITimer.attachInterruptInterval(TIMER_INTERVAL_MS * 1000, func)){
-	// 	#ifdef DEBUG
-	// 		Serial.println("ERROR: Periodical Wireless transmition failed");
-	// 	#endif
-	// 	return -1; 
-	// }
-	// #ifdef DEBUG
-	// else {
-	// 	Serial.print("Communication timer configured to ");
-	// 	Serial.print(TIMER_INTERVAL_MS);
-	// 	Serial.println(" msec");
-	// }
-	// #endif
 	timerAttachInterrupt(this->timer_cfg, func, EDGE);
-	// timerAlarmWrite(this->timer_cfg, PRELOAD, AUTO_RELOAD);
 	timerAlarmEnable(this->timer_cfg);
 	#ifdef DEBUG
 	Serial.println("Interrupt configured");
 	#endif
 	
-    // timerAttachInterrupt(this->timer_cfg, func, true);
-    // timerAlarmEnable(this->timer_cfg);
 
 	return 0; // No error
 }
+
+
+// void TimerInterruption::ISR(void){
+//   digitalWrite(2,!digitalRead(2));
+//   request_to_send = true;
+// }
