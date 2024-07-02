47a3d0e94a4756a073d798f3059add50c09e841f
Rafael Ramildes Ferreira <rafael.r.ferreira@grad.ufsc.br>
2024-07-02 16:34:25 -0300

    Adds flag for defining if frequency is to be calculated on ESP

--- include/chest_compressions.h
+++ include/chest_compressions.h
@@ -2,22 +2,31 @@
 #define _CHEST_COMPRESSION_H
 
 #include <Adafruit_VL6180X.h>
+#include "buildconfig.h"
 
 
 class ChestCompression{
 	public:
-		ChestCompression();
-		double calc_frequency();
+    	int begin();
 		double calc_distance();
 		double get_distance();
+	#ifdef FREQUENCY_ON_ESP
+		double calc_frequency();
 		double get_frequency();
+	#endif
 
 	private:
+	#ifdef FREQUENCY_ON_ESP
 		double last_peak(double distance);
 		double last_valley(double distance);
 
+    	double mean,deviation;
+    	double last_valley_time,last_peak_time;
+		double frequency;
+	#endif
+
 		Adafruit_VL6180X dist_sensor;
-		double distance, frequency;
+		double distance;
 };
 
 #endif
